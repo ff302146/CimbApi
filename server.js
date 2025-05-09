7c4dd582235d3844c43e85dd27d2508d66878877
Paydaysite1 <Paydaysite1@gmail.com>
2025-05-09 18:03:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "Paydaysite1@gmail.com";
 const pass = "jrqjapmxebtahohb";
+// 19th may
+
 
 // Middleware
 app.use(express.json());
